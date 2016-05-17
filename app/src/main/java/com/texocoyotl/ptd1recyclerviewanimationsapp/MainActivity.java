d386c8e1ea9c5d7b096e6c2f7ef68333e57794ad
Benjamin Molina <gaeldron.might@gmail.com>
2016-05-17 18:31:33 -0400

    Added fragment with recyclerview

--- app/src/main/java/com/texocoyotl/ptd1recyclerviewanimationsapp/MainActivity.java
+++ app/src/main/java/com/texocoyotl/ptd1recyclerviewanimationsapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.texocoyotl.ptd1recyclerviewanimationsapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.texocoyotl.ptd1recyclerviewanimationsapp.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Toast.makeText(this, item.toString(), Toast.LENGTH_SHORT).show();
     }
 }
